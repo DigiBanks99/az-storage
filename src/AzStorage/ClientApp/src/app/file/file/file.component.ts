356ec82bc1d2b68e77e57ce4603e1356daa4d22b
Wilco Boshoff <wlboshoff@gmail.com>
2023-09-01 13:28:20 +0200

    Add video playback

--- src/AzStorage/ClientApp/src/app/file/file/file.component.ts
+++ src/AzStorage/ClientApp/src/app/file/file/file.component.ts
@@ -17,6 +17,7 @@ export class FileComponent {
   public form: FormGroup = new FormGroup<FileForm>({
     file: new FormControl<File | null>(null, {nonNullable: true})
   });
+  public videoFile: FileInfo | null = null;
 
   private fetchFiles$ = new BehaviorSubject<number>(0);
 
@@ -24,13 +25,15 @@ export class FileComponent {
     this.fileInfos$ = this.fetchFiles$.pipe(switchMap(() => this.fileService.getAll().pipe(take(1))));
   }
 
+  public checkIfVideo(file: FileInfo): boolean {
+    return file.contentType.startsWith('video/');
+  }
+
   public getDownloadUri(file: FileInfo): string {
     return `/api/file/${file.name}`;
   }
 
-  public onSubmit(): void {
-    if (this.form.valid) {
-      this.fileService.upload(this.form.value.file).pipe(take(1)).subscribe(() => this.fetchFiles$.next(this.fetchFiles$.value + 1));
-    }
+  public onPlayVideo(file: FileInfo): void {
+    this.videoFile = file;
   }
 }
